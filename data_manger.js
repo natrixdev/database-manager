e3cc16c45ebf57a0bbf5ef1f60ff89edc2f5e308
natrix_dev <discordlenko@gmail.com>
2021-10-07 19:46:30 +0200

    Create data_manger.js
    New js manager using my npm module (natrix)
    npm init 
    npm i natrix --save
    npm i natrix
    npm i natrix-databases

---
+++ data_manger.js
@@ -0,0 +1,22 @@
+<= js.
+const data = reuqire("natrix")
+const ip = require("natrix-ip-owner")
+
+ip.
+manager.
+
+let users = natrix.fetch("users") 
+let passwords = natrix.passes.data(DATA)
+
+log()
+  log 
+     chalk.GREEN("Users : $$users" + "Passwords : $$passwords")
+     
+   //exports
+   
+   natrix.export();
+   
+   
+data.DATA(end) 
+
+.js =>
